936ddb1e7a80e2e34bccbe74c6ae6a0e21ad0dc4
Dante4K18 <daniel.ombui@student.moringaschool.com>
2024-07-11 03:26:02 +0300

    Completed JavaScript Functions

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
-// code your solution here
+const mondayWork = function(activity = "go to the office") {
+    return `This Monday, I will ${activity}.`;
+};
+
+console.log(mondayWork("work from home")); 
+console.log(mondayWork());
+
+function saturdayFun(activity = "roller-skate") {
+    return `This Saturday, I want to ${activity}!`;
+}
+
+console.log(saturdayFun()); 
+console.log(saturdayFun("bathe my dog")); 
+
+function wrapAdjective(wrapper = "*") {
+    return function(adjective = "special") {
+        return `You are ${wrapper}${adjective}${wrapper}!`;
+    };
+}
+
+const wrapWithStars = wrapAdjective('*');
+console.log(wrapWithStars("a hard worker")); 
+
+const wrapWithPipes = wrapAdjective('||');
+console.log(wrapWithPipes("a dedicated programmer")); 
